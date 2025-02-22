4d27f28e22c8a1cc61bdef4f22950fe6fcdd74e7
Raymond Lei(雷菩宇); 䨻-Ficus religiosa-Cousin <ray810815@gmail.com>
2025-02-22 02:09:00 -0500

    Update index.tsx
    ```typescript
    import LunarCalendar from './src/lunar-calendar';
    
    document.addEventListener('DOMContentLoaded', () => {
      new LunarCalendar('lunar-calendar');
    });
    
    export default LunarCalendar;
    ```

--- Typescript/Lunar Calendar/index.tsx
+++ Typescript/Lunar Calendar/index.tsx
@@ -1,1 +1,7 @@
+import LunarCalendar from './src/lunar-calendar';
 
+document.addEventListener('DOMContentLoaded', () => {
+  new LunarCalendar('lunar-calendar');
+});
+
+export default LunarCalendar;
